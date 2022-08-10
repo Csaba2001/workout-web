f8ab7d5432e149c7f35aefa371e537c93846e95e
Csaba <dobocsaba13gmail.com>
2022-08-11 01:28:41 +0200

    cv, new exercise insert

---
+++ newtraining.php
@@ -0,0 +1,36 @@
+<?php
+@session_start();
+require_once("db_config.php");
+require_once("functions.php");
+
+if(!isTrainer()) {
+    redirect("index.php?page=home");
+}
+if (isPost() && !empty($_POST)) {
+    addtraining();
+} else {
+    try {
+        $_POST = json_decode(file_get_contents("php://input"), true);
+        addexercise();
+    } catch (Exception $e) {
+        json("Not a POST request");
+    }
+}
+
+function addtraining(){
+    global $dbh;
+    $exerciseName = sanitize($_POST["ExerciseName"]);
+    $description = sanitize($_POST["Description"]);
+    $trainerID = $_SESSION['PersonID'];
+
+    try {
+        $sql = "INSERT INTO exercises (ExerciseName, Description, TrainerID) VALUES (:en,:dc,:tid)";
+        $query = $dbh->prepare($sql);
+        $query->bindParam(':en', $exerciseName);
+        $query->bindParam(':dc', $description);
+        $query->bindParam(':tid', $trainerID);
+        $query->execute();
+    } catch (PDOException $error) {
+        die($error);
+    }
+}
--- pages/workout.php
+++ pages/workout.php
@@ -35,76 +35,100 @@ try {
     echo "</pre>";*/
 ?>
 
-<div class="searchbar" style="overflow: scroll; width: 100%; max-width: 100%;">
-    <form method="post">
-        <table class="d-flex table table-dark table-striped" style=" text-align: center; overflow: scroll;" >
-            <tr>
-                <th scope="col">Kategória</th>
-                <th scope="col">Edzés leírása</th>
-                <th scope="col">Hétfő</th>
-                <th scope="col">Kedd</th>
-                <th scope="col">Szerda</th>
-                <th scope="col">Csütörtök</th>
-                <th scope="col">Péntek</th>
-                <th scope="col">Szombat</th>
-                <th scope="col">Vasárnap</th>
-                <th scope="col">Értékelés</th>
-                <th scope="col">Hozzáadás</th>
-            </tr>
-            <tr>
-                <td><br/><select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" style="line-height: 30px; margin-right: 30px">
-                        <option value="nocateg">Kategória</option>
-                        <option value="weightloss">Fogyás</option>
-                        <option value="cutting">Szálkásítás</option>
-                        <option value="bulking">Erősítés</option>
-                    </select></td>
 
-                <td><textarea class="form-control" aria-label="With textarea"></textarea></td>
-                <td><textarea class="form-control" aria-label="With textarea"></textarea></td>
-                <td><textarea class="form-control" aria-label="With textarea"></textarea></td>
-                <td><textarea class="form-control" aria-label="With textarea"></textarea></td>
-                <td><textarea class="form-control" aria-label="With textarea"></textarea></td>
-                <td><textarea class="form-control" aria-label="With textarea"></textarea></td>
-                <td><textarea class="form-control" aria-label="With textarea"></textarea></td>
-                <td><textarea class="form-control" aria-label="With textarea"></textarea></td>
-                <td><textarea class="form-control" aria-label="With textarea"></textarea></td>
-                <td><button name='insert' class="btn btn-danger" >INSERT</button></td>
-            </tr>
-        </table>
-        <table class="table table-dark table-striped" style=" text-align: center; overflow: scroll; " >
-            <tr>
-                <th scope="col">Kategória</th>
-                <th scope="col">Edzés leírása</th>
-                <th scope="col">Hétfő</th>
-                <th scope="col">Kedd</th>
-                <th scope="col">Szerda</th>
-                <th scope="col">Csütörtök</th>
-                <th scope="col">Péntek</th>
-                <th scope="col">Szombat</th>
-                <th scope="col">Vasárnap</th>
-                <th scope="col">Értékelés</th>
-                <th scope="col">&nbsp;Törlés</th>
-            </tr>
-            <?php
-                if(isset($results))
-                    foreach ($results as $result) {
-                        echo "<tr>
-                
-
-                <td>$result[0]</td>
-                <td onclick=openWin()>Edzés leírása</td>
-                <td>$result[3]</td>
-                <td>$result[4]</td>
-                <td>$result[5]</td>
-                <td>$result[6]</td>
-                <td>$result[7]</td>
-                <td>$result[8]</td>
-                <td>$result[9]</td>
-                <td>$result[2]</td>
-                <td><button name='delete' class='btn btn-danger' >DELETE</button></td>
-              </tr>";
-                    }
-            ?>
+<?php if(isTrainer()) : //only trainer ?>
+<div class="searchbar" style="width: 100%; max-width: 100%;">
+    <?php if(isset($exercises) && !empty($exercises)): ?>
+    <form id="newTrainingForm" name="newTrainingForm" action="newtraining.php" method="post" enctype="application/x-www-form-urlencoded">
+        <table class="table table-sm table-dark table-striped" style="text-align: center;">
+            <thead>
+                <tr>
+                    <th scope="col">Kategória</th>
+                    <th scope="col">Edzés leírása</th>
+                    <th scope="col">Hétfő</th>
+                    <th scope="col">Kedd</th>
+                    <th scope="col">Szerda</th>
+                    <th scope="col">Csütörtök</th>
+                    <th scope="col">Péntek</th>
+                    <th scope="col">Szombat</th>
+                    <th scope="col">Vasárnap</th>
+                    <th scope="col">Hozzáadás</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <select id="trainingCategory" name="trainingCategory" class="form-select" aria-label=".form-select-lg">
+                            <option value="nocateg">Kategória</option>
+                            <option value="weightloss">Fogyás</option>
+                            <option value="cutting">Szálkásítás</option>
+                            <option value="bulking">Erősítés</option>
+                        </select>
+                    </td>
+                    <td>
+                        <textarea></textarea>
+                    </td>
+                    <?php for($i = 0; $i < 7; $i++): ?>
+                    <td>
+                        <select id="<?= $days[$i] ?>" name="<?= $days[$i] ?>">
+                            <option value="noexec">Gyakorlat</option>
+                            <?php foreach($exercises as $exercise) : ?>
+                            <option value="<?= $exercise["ExerciseID"] ?>"><?= $exercise["ExerciseName"] ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </td>
+                    <?php endfor; ?>
+                    <td>
+                        <input type="submit" class="btn btn-danger" value="Letrehoz">
+                    </td>
+                </tr>
+            </tbody>
         </table>
     </form>
+    <?php else : ?>
+    <span>Nincs egy gyakorlata sem, keszitsen parat...</span>
+    <a href="index.php?page=execa" class="btn btn-primary">Uj gyakorlat</a>
+    <?php endif; ?>
 </div>
+
+<?php elseif(isUser()) : //only users ?>
+<table class="table table-small table-dark table-striped" style=" text-align: center;" >
+    <tr>
+        <th scope="col">Kategória</th>
+        <th scope="col">Edzés leírása</th>
+        <th scope="col">Hétfő</th>
+        <th scope="col">Kedd</th>
+        <th scope="col">Szerda</th>
+        <th scope="col">Csütörtök</th>
+        <th scope="col">Péntek</th>
+        <th scope="col">Szombat</th>
+        <th scope="col">Vasárnap</th>
+        <th scope="col">Értékelés</th>
+        <th scope="col">&nbsp;Törlés</th>
+    </tr>
+    <?php
+        /*if(isset($exercises)){
+            foreach ($exercises as $exercise) {
+                echo "
+                <tr>
+                    <td>$result[0]</td>
+                    <td onclick=openWin()>Edzés leírása</td>
+                    <td>$result[3]</td>
+                    <td>$result[4]</td>
+                    <td>$result[5]</td>
+                    <td>$result[6]</td>
+                    <td>$result[7]</td>
+                    <td>$result[8]</td>
+                    <td>$result[9]</td>
+                    <td>$result[2]</td>
+                    <td><button name='delete' class='btn btn-danger' >DELETE</button></td>
+                </tr>";
+            }
+        } else {
+            echo "
+            <tr>
+                <td colspan=\"8\">Nincs egy gyakorlata sem, keszitsen parat...</td>
+            </tr>";
+        } */?>
+</table>
+<?php endif; ?>
